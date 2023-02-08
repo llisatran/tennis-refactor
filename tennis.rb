ffd5c3ff7221c01209ca33b2cf5f2d525c2cdeb4
llisatran <lisatpriv@gmail.com>
2023-02-08 14:30:18 +0100

    finalizing task 1

--- tennis.rb
+++ tennis.rb
@@ -1,3 +1,12 @@
+class Player
+  attr_reader :name
+  attr_accessor :points
+  def initialize(name)
+    @name = name
+    @points = 0
+  end
+end
+
 class TennisGame1
   SCORE_HASH = {
     0 => "Love",
@@ -7,58 +16,46 @@ class TennisGame1
   }.freeze
 
   def initialize(player1Name, player2Name)
-    @player1Name = player1Name
-    @player2Name = player2Name
-    @p1points = 0
-    @p2points = 0
+   @p1 = Player.new(player1Name)
+   @p2 = Player.new(player2Name)
   end
         
   def won_point(playerName)
-    if playerName == "player1"
-      @p1points += 1
+    if playerName == @p1.name
+      @p1.points += 1
     else
-      @p2points += 1
+      @p2.points += 1
     end
   end
 
   def leading_player
-    if @p1points > @p2points
-      return @player1Name
-    else 
-      return @player2Name
-    end
+    return @p1.name if @p1.points > @p2.points
+    @p2.name
   end
 
   def even_score_output(score)
-    if score <= 2
-      "#{SCORE_HASH[score]}-All"
-    else 
-      "Deuce"
-    end
+    return "#{SCORE_HASH[score]}-All" if score <= 2
+    "Deuce"
   end
 
   def win_or_advantage_output(diff_score)
-    return "Advantage #{leading_player()}" if (diff_score==1)
-    "Win for #{leading_player()}" if (diff_score>=2)
+    return "Advantage #{leading_player}" if (diff_score==1)
+    "Win for #{leading_player}" if (diff_score>=2)
   end
 
   def game_progress_output
-    "#{SCORE_HASH[@p1points]}-#{SCORE_HASH[@p2points]}"
+    "#{SCORE_HASH[@p1.points]}-#{SCORE_HASH[@p2.points]}"
   end
   
   def score
-    result=""
-    tempScore=0
-    
-    if (@p1points==@p2points)
-      result = even_score_output(@p1points)
-    elsif ([@p1points, @p2points].max >= 4)
-      diff_score = (@p1points-@p2points).abs
-      result = win_or_advantage_output(diff_score)
+    if (@p1.points==@p2.points)
+      even_score_output(@p1.points)
+    elsif (@p1.points >= 4 || @p2.points >= 4)
+      diff_score = (@p1.points-@p2.points).abs
+      win_or_advantage_output(diff_score)
     else
-      result = game_progress_output
+      game_progress_output
     end
-    result
   end
 end
 
